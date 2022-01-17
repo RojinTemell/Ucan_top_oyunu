c0c52ae00600f9d9be72405cc8b2b3f18acdf36b
RojinTemell <94474533+RojinTemell@users.noreply.github.com>
2022-01-17 22:57:02 +0300

    Add files via upload

---
+++ Ucan_top_oyunu/Ucan_top_oyunu/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace Ucan_top_oyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.sol_kolon = new System.Windows.Forms.Label();
+            this.taban = new System.Windows.Forms.Label();
+            this.sag_kolon = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // sol_kolon
+            // 
+            this.sol_kolon.BackColor = System.Drawing.Color.LimeGreen;
+            this.sol_kolon.Location = new System.Drawing.Point(38, 46);
+            this.sol_kolon.Name = "sol_kolon";
+            this.sol_kolon.Size = new System.Drawing.Size(39, 540);
+            this.sol_kolon.TabIndex = 0;
+            // 
+            // taban
+            // 
+            this.taban.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.taban.Location = new System.Drawing.Point(38, 46);
+            this.taban.Name = "taban";
+            this.taban.Size = new System.Drawing.Size(864, 41);
+            this.taban.TabIndex = 2;
+            // 
+            // sag_kolon
+            // 
+            this.sag_kolon.BackColor = System.Drawing.Color.LimeGreen;
+            this.sag_kolon.Location = new System.Drawing.Point(863, 46);
+            this.sag_kolon.Name = "sag_kolon";
+            this.sag_kolon.Size = new System.Drawing.Size(39, 540);
+            this.sag_kolon.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(416, 157);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(35, 25);
+            this.button1.TabIndex = 3;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.button2.Location = new System.Drawing.Point(340, 554);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 32);
+            this.button2.TabIndex = 4;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Green;
+            this.button3.Location = new System.Drawing.Point(944, 62);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(35, 35);
+            this.button3.TabIndex = 5;
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Green;
+            this.button4.Location = new System.Drawing.Point(994, 62);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(34, 35);
+            this.button4.TabIndex = 6;
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Green;
+            this.button5.Location = new System.Drawing.Point(1043, 62);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(34, 35);
+            this.button5.TabIndex = 7;
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(1102, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 35);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "3";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1177, 663);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.sag_kolon);
+            this.Controls.Add(this.taban);
+            this.Controls.Add(this.sol_kolon);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label sol_kolon;
+        private System.Windows.Forms.Label taban;
+        private System.Windows.Forms.Label sag_kolon;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Ucan_top_oyunu/Ucan_top_oyunu/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ucan_top_oyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int Xekseni = 3, Yekseni = 3 ,can ;
+        private void Calisma_olayi()
+        {  
+            if (button1.Top <= taban.Bottom)
+            {
+                Yekseni *= -1;
+            }
+            if(button1.Right >= sag_kolon.Left)
+            {
+                Xekseni *= -1;
+            }
+            if (button1.Left <=sol_kolon.Right)
+            {
+                Xekseni *= -1;
+            }
+            if (button1.Bottom >=button2.Top && button1.Left>=button2.Left && button1.Right<=button2.Right)
+            {
+                Yekseni *= -1;
+            }
+
+        }
+        private void Topun_baslama_noktasi(object sender, EventArgs e)
+        {
+            button1.Location = new Point(416 ,  157 );
+        }
+        private void Yanma_olayi(object sender, EventArgs e)
+        {
+           
+          
+     
+        }       
+      
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e )
+        {
+           
+            button2.Left = e.X;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e )
+        {
+            timer1.Enabled = true;
+            Topun_baslama_noktasi(sender,e);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e )
+        {
+            Calisma_olayi();
+            Yanma_olayi(sender, e );
+            button1.Location = new Point(button1.Location.X + Xekseni , button1.Location.Y +Yekseni );
+             
+        }
+
+      
+    }
+}
+
